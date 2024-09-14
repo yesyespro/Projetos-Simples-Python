eef28d9fd9d020c40164866c1b93473127e848e7
BALSpro <robertocarmario308@gmail.com>
2024-09-14 13:00:08 -0300

    Terminado em um dia

--- hello.py
+++ hello.py
@@ -1,9 +1,32 @@
 from colorama import Fore
 import sys
 from os import system
+import os
+import time
+
+def verificar_vogais(conteudo):
+    vogais= ["a","e","i","o","u"] 
+    i=0
+    for linha in conteudo:
+        if linha in vogais: #verificando se caractere esta no array vogais
+            i+=1
+    return i
+        
+
 
 if __name__ == "__main__":
-    system("clear"); # Limpando a tela
+    system("clear"); 
     if len(sys.argv) <= 2:
         file=sys.argv[1]
-        print(file)
+        if not os.path.isfile(file):
+            print("Por favor passe um arquivo")
+            exit(1)
+        #lendo arquivo e mandando pra função verificar vogais
+        with open(file, 'r') as arq:
+            ler=arq.read()
+            retorno = verificar_vogais(ler)
+        if retorno > 1:
+            plural="vogais"
+        else:
+            plural="vogal"
+        print(f"há {retorno} {plural}")
